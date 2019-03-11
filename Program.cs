b76fd477c4eeb20683e17d94ca7c15515d4745b6
MD.Jahid Hassan <32840051+polashbabu@users.noreply.github.com>
2019-03-11 10:21:32 +0600

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Sealed_class
+{
+    sealed class sesalclas
+    { public int Add(int a, int b)
+        {
+           
+            return a + b;
+        }
+       }
+     
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            sesalclas ob = new sesalclas();
+            ob.Add(10, 20);
+            Console.WriteLine(ob.Add(10, 20));
+            Console.ReadKey();
+        }
+    }
+}
